76ba56cffe4e5081235a7d3798c49f4620702fd6
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 23:35:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
-
+  attr_accessor :status, :balance
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(number)
+    @balance += number
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    if @balance > 0 && @status == "open"
+      puts "VALID - Account #{@name} has a balance of #{balance} with a status of #{status}"
+      return true 
+    else
+      puts "NOT VALID - Account #{@name} has a balance of #{balance} with a status of #{status}"
+      return false 
+    end
+  end
+  
+  def close_account
+    @balance = 0 
+    @status = "closed"
+  end
 end
