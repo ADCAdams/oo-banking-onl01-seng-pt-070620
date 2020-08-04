d4a9388d609b54035e52fe6929c8d857e9e2b722
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 23:23:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,11 @@ class Transfer
         @receiver.balance += @amount
         @status = "complete"
       else 
-        puts "#{@sender} NOT PENDING"
+        puts "#{@sender.name} NOT PENDING"
         @status = "Transaction rejected. Please check your account balance."
       end
     else 
-      puts "#{@sender} NOT VALID"
+      puts "#{@sender.name} NOT VALID"
       @status = "Transaction rejected. Please check your account balance."
     end
   end
