67d85cffdabd4758eff6d79715f645262114a90a
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 23:34:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,6 @@ class Transfer
           @sender.balance -= @amount
           @receiver.balance += @amount
           @status = "complete"
-          
         else
           @status = "rejected"
           "Transaction rejected. Please check your account balance."
@@ -32,4 +31,12 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+  
+  def reverse_transfer 
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+    end
+  end
+  
 end
