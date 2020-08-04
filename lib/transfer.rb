93d86dccf5a0a289c27edb24344418d43700d78c
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 22:19:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
 class Transfer
-  attr_accessor :name 
-  def initialize(name)
+  attr_accessor :name, :receiver, :amount
+  def initialize(name, receiver, amount)
     @name = name
+    @receiver = receiver
+    @amount = amount
   end
 end
